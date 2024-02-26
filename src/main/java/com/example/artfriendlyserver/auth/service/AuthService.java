6c8aa813ac7c22f3da98cc510ee1897a65df6adb
redandsilver <129943670+redandsilver@users.noreply.github.com>
2024-02-26 18:20:16 +0900

    Update AuthService.java

--- src/main/java/com/example/artfriendlyserver/auth/service/AuthService.java
+++ src/main/java/com/example/artfriendlyserver/auth/service/AuthService.java
@@ -12,17 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
     private final KakaoApi kakaoApi;
-    private final UserRepository userRepository;
     public String kakoLogin (String code) throws JsonProcessingException {
         return kakaoApi.login(code);
     }
     public String validateUser (String token) throws JsonProcessingException {
-        User user = User.parseFrom(kakaoApi.getUser(token));
-        if(userRepository.findByKakaoId(user.getKakaoId()).isEmpty()){
-            userRepository.save(user);
-            return "신규회원. 관심 분야 선택 진행";
-        }
-        return "기존 회원";
+        return kakaoApi.login(code);
     }
 
     public String kakoLogout (String token) throws JsonProcessingException {
