3793f61431faa9e767cdd3466e3425f8ab10b31e
jrnecki <129943670+jrnecki@users.noreply.github.com>
2024-02-26 14:40:41 +0900

    Initial commit

---
+++ src/main/java/com/example/artfriendlyserver/ArtFriendlyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.artfriendlyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtFriendlyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtFriendlyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artfriendlyserver/ArtFriendlyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artfriendlyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtFriendlyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
